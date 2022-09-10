7fd7453c1f8c80c3f5f5dd0692cbd59c4784d8e5
Arturo González Peñaloza <arturognz@ciencias.unam.mx>
2022-09-10 13:22:35 -0500

    Se modificó handler de acceso y agregó plantillas html.

--- servidor/servidor.go
+++ servidor/servidor.go
@@ -1,31 +1,71 @@
 package servidor
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 	"text/template"
+
+	"github.com/Artgnz/Modelado-y-Programacion-Proyecto-01/servidor/clima"
 )
 
-// EjecutarServidor ejecuta un servidor en el puerto 8080.
+// EjecutarServidor ejecuta el servidor en el puerto 8080.
 func EjecutarServidor() {
 	puerto := "8080"
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", funcionHandle)
+	var cliente *clima.ClienteClima
+	mux.HandleFunc("/", funcionHandle(cliente))
 	http.ListenAndServe("localhost:"+puerto, mux)
+
 }
 
-// funcionHandle decide que función usar para procesar la petición r y
-// para construir una respuesta HTTP con w.
-func funcionHandle(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL)
-	switch r.URL.Path {
-	case "/acceso/":
-		acceso(w, r)
+// funcionHandle recibe un cliente de openWeather y devuelve una función
+// handler para procesar una petición r y construir una respuesta HTTP con w.
+func funcionHandle(cliente *clima.ClienteClima) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/acceso/":
+			cliente = accesoHandler(w, r)
+		case "/tickets/":
+			ticketsHandler(cliente, w, r)
+		}
 	}
 }
 
-// acceso procesa peticiones relacionadas con el ingreso de sesión.
-func acceso(w http.ResponseWriter, r *http.Request) {
-	tpl := template.Must(template.ParseFiles("assets/templates/acceso.html"))
+// ticketsHandler procesa peticiones relacionadas con mostrar la tabla de tickets.
+// Recibe cliente para realizar peticións a openweather.
+func ticketsHandler(cliente *clima.ClienteClima, w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("assets/templates/tickets.html"))
 	tpl.Execute(w, nil)
+}
+
+// accesoHandler procesa peticiones relacionadas con el ingreso de sesión.
+func accesoHandler(w http.ResponseWriter, r *http.Request) *clima.ClienteClima {
+	switch r.Method {
+	// Si el método de la petición es GET
+	case "GET":
+		tpl := template.Must(template.ParseFiles("assets/templates/acceso.html"))
+		tpl.Execute(w, nil)
+		// Si el método de la petición es POST
+	case "POST":
+		// Se lee la llave API de la petición
+		llaveApi := r.FormValue("llave-api")
+		// Se crea un cliente con la llave.
+		cliente, error := clima.NuevoClienteClima(&http.Client{}, llaveApi)
+		// Si hubo un error.
+		if error != nil {
+			// Se crea plantilla para llave incorrecta.
+			tpl := template.Must(template.ParseFiles("assets/templates/llave-incorrecta.html"))
+			// Se ejecuta la plantilla
+			tpl.Execute(w, nil)
+		} else {
+			// Se redirecciona a url de tickets.
+			http.Redirect(w, r, "/tickets/", http.StatusFound)
+			// Se regresa el cliente.
+			return cliente
+		}
+	default:
+		// La url solo soporta los métodos GET y POST
+		fmt.Fprintf(w, "Soporte únicamente para métodos GET y POST.")
+	}
+	return nil
 }
